89525d9096f1137192d33687a7843619c30c5aec
adamz <58021562+ad4mny@users.noreply.github.com>
2021-09-22 23:28:53 +0800

    added all pages boilerplate

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -80,54 +80,6 @@ var login = function () {
     }
 };
 
-var register = function () {
-
-    var name = $("#name").val();
-    var contact = $("#contact").val();
-    var username = $("#username").val();
-    var password = $("#pwd").val();
-
-    if (comparePassword() != false) {
-
-        var input = {
-            "name": name,
-            "contact": contact,
-            "username": username,
-            "password": password
-        };
-
-        $.ajax({
-            type: "POST",
-            url: web_links + "api/get_signup",
-            data: input,
-            dataType: 'JSON',
-            success: function (data) {
-                if (data == false) {
-                    alert('Error signing you up.');
-                } else if (data == "Username unavailable, register again.") {
-                    alert(data);
-                } else {
-                    localStorage.setItem('users', JSON.stringify(data));
-                    location.replace('index.html');
-                }
-            }
-        });
-
-    }
-
-};
-
-var comparePassword = function () {
-
-    if ($('#pwd').val() != $('#c_pwd').val()) {
-        $('#c_pwd').addClass("border border-danger");
-        return false;
-    } else {
-        $('#c_pwd').removeClass("border border-danger");
-        return true;
-    }
-};
-
 var logout = function () {
 
     localStorage.clear();
